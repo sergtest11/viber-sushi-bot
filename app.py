e15466157e8ec6bab522003487e6e41961c53c73
sergtest11 <83017350+sergtest11@users.noreply.github.com>
2021-04-22 17:08:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def incoming():
         keyboard = kb.SHARE_PHONE_KEYBOARD
         viber.send_messages(viber_request.user.id, [
             TextMessage(
-                text=txt.GREETING + '\n' + str(viber_request.user.id),
+                text=txt.GREETING,
                 keyboard=keyboard,
                 min_api_version=3)
             ]
